baba058520a165e4837132d6116d1480afe79455
Fatimatuzzohramgadwal <rubamgadwal83@gmail.com>
2026-08-10 19:39:02 +0530

    serve frontend from backend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className="container home-page">
+    <div style={{ padding: "40px", textAlign: "center" }}>
       <h1>Hello, I'm a Full Stack Developer 🚀</h1>
 
-      <p>
+      <p style={{ marginTop: "10px" }}>
         Building modern web applications with MERN stack
       </p>
 
       <Link to="/projects">
-        <button className="btn">
+        <button style={{ marginTop: "20px", padding: "10px 20px" }}>
           View Projects
         </button>
       </Link>
